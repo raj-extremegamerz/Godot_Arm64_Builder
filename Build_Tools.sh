758014c327d5f6cf49dd1d83d04156e1bd3603b2
Extreme Gamerz <93861794+raj-extremegamerz@users.noreply.github.com>
2022-12-15 21:55:27 +0530

    Update Build_Tools.sh

--- Build_Tools.sh
+++ Build_Tools.sh
@@ -13,8 +13,8 @@ git clone https://github.com/godotengine/godot.git
 clear
 echo Starting Building
 cd ~/godot
-scons platform=linuxbsd arch=arm64 tools=yes target=template_debug use_llvm=no -j8 ; scons -c # 8 threads used because most SBCs have between 4-8 cores. This will make sure they are all used for building.
-strip ~/godot/bin/*tools.arm64
+scons platform=linuxbsd arch=arm64 target=template_debug use_llvm=no -j8
+strip ~/godot/bin/*arm64
 echo Build Finished
 ls -a 
 ls -a bin/
